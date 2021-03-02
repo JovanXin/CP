496dc79f39b17f569b9906fcea7a8af12eb84990
JovanXin <jovan3.1415926@gmail.com>
2021-03-02 15:34:26 +1300

    Fix bug :|

--- NZOI/2020/2020_r1_2_road.py
+++ NZOI/2020/2020_r1_2_road.py
@@ -3,8 +3,8 @@ facilities = list(map(int, input().split()))
 
 distance = int(input())
 
-distances = sorted(min(distance % x, x - distance) for x in facilities)
+distances = sorted(min(distance % x, x - distance % x) for x in facilities)
 
-print(distance[0])
+print(distances[0])
 if distances[0] == distances[1]:
     print("can't make up my mind")
