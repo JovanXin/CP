7af53c13385b056655e243ff43aa41b6e0968778
JovanXin <jovan3.1415926@gmail.com>
2021-03-28 22:45:14 +1300

    Add inefficent non-memoized O(2^n?) solution for coin summation

--- CSES/Book/coin.py
+++ CSES/Book/coin.py
@@ -2,25 +2,30 @@
 Given a list of coin values and a target number, return the minimum amount of coins required 
 to reach that target.
 
+Current solution is probably O(2^n?) cause of the extra branches repeated...
 Extra -> Return # of possible ways to reach the target num.
 """
 
-coins = (1, 3, 4)
-SUM = 10
-
 # Best is getting redefined as infinity every time? something about scope?? idfk
 
+import math
 
-def work(sum):
-    if sum == SUM:
-        best = float("inf")
-    if sum == 0:
+coins = (1, 3, 4)
+
+
+def coin(target):
+    if target < 0:
+        return float("inf")
+    elif target == 0:
         return 0
 
+    best = float("inf")
     for c in coins:
-        best = max(best, (work(sum - c) + 1))
-        return best
+        best = min(
+            best, coin(target - c) + 1
+        )  # +1 cause we have to count the current coin that's been subtracted
+
+    return best
 
 
-x = work(SUM)
-print(x)
+print(coin(int(input())))
