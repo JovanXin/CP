4bd226cc650b1514704bfd9a449b4c870a1c007f
JovanXin <jovan3.1415926@gmail.com>
2021-03-28 22:50:53 +1300

    Add memoized solution for coin summation

--- CSES/Book/coin.py
+++ CSES/Book/coin.py
@@ -2,28 +2,31 @@
 Given a list of coin values and a target number, return the minimum amount of coins required 
 to reach that target.
 
-Current solution is probably O(2^n?) cause of the extra branches repeated...
-Extra -> Return # of possible ways to reach the target num.
+Recursive solution using memoization to save the min coins required for each amount
+
+TODO -> Figure out time complexity of this solution
 """
 
-# Best is getting redefined as infinity every time? something about scope?? idfk
 
 import math
 
 coins = (1, 3, 4)
+memoization = {}
 
 
 def coin(target):
     if target < 0:
         return float("inf")
+    elif target in memoization.keys():
+        return memoization[target]
     elif target == 0:
         return 0
 
     best = float("inf")
     for c in coins:
-        best = min(
-            best, coin(target - c) + 1
-        )  # +1 cause we have to count the current coin that's been subtracted
+        # +1 cause we have to count the current coin that's been subtracted
+        best = min(best, coin(target - c) + 1)
+        memoization[target] = best
 
     return best
 
