0621d7e75fa9c7440ec548c5ea4acca648441f53
JovanXin <jovan3.1415926@gmail.com>
2021-03-29 22:31:15 +1300

    Add broken attempt to get coins summing to solution

--- CSES/Book/coin_interative.py
+++ CSES/Book/coin_interative.py
@@ -1,20 +1,29 @@
 """
 Same as the previous coin problem, except this time solved recursively
+
+Added the coins needed to reach that amount -> kinda broken
 """
 
 coins = (1, 3, 4)
 
 
 def coin(target):
+    required_coins = []
     values = [0]
-    for i in range(1, target):
+    for i in range(1, target + 1):
         values.append(float("inf"))
 
         for c in coins:
-            if i - c >= 0:
-                values[i] = min(values[i], values[i - c] + 1)
+            if i - c >= 0 and values[i - c] + 1 < values[i]:
+                values[i] = values[i - c] + 1
+                required_coins.append(c)
 
-    return values[target - 1]  # Return index at 4
+    n = len(required_coins)
+    while n > 0:
+        print(required_coins[n - 1], end=", ")
+        n -= required_coins[n - 1]
+
+    return values[target]  # Return index at 4
 
 
 print(coin(int(input())))
