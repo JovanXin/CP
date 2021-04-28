6404b7b4bd9c10c87e301adf57068e078d067fcd
JovanXin <jovan3.1415926@gmail.com>
2021-04-28 21:14:52 +1200

    Add 1d knapsack implementation

--- Types/knapsack.py
+++ Types/knapsack.py
@@ -1,7 +1,19 @@
 from typing import List
 
 
-def knapsack(weights: List[int], values: List[int], max_weight: int):
+def knapsack_1d(weights: List[int], values: List[int], max_weight: int) -> int:
+    """ Returns the max value possible """
+    dp = [0] * (max_weight + 1)
+    for weight, value in zip(weights, values):
+        dp = dp[:weight] + [
+            max(dp[i + weight], dp[i] + value) for i in range(max_weight + 1 - weight)
+        ]
+
+    return max(dp)
+
+
+def knapsack(weights: List[int], values: List[int], max_weight: int) -> List[int]:
+    """ Returns position (index + 1) for all knapsack elements used """
     dp = [[0] * (max_weight + 1) for _ in range(len(values) + 1)]
 
     # Implementation of tabulation
@@ -39,4 +51,7 @@ weights = [3, 1, 3, 4, 2]
 values = [2, 2, 4, 5, 3]
 
 used_vals = knapsack(weights, values, 7)
+max_vals = knapsack_1d(weights, values, 7)
+
 print(used_vals)  # Items 5, 4 and 2 should be optimally used
+print(max_vals)  # Should be 10
